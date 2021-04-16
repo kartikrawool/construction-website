eb1246af83402be75ebd9b8dbe01f66401c6c394
Aayu231 <aayushi.sarda@gmail.com>
2021-04-16 14:25:48 +0530

    Login & Register

--- src/app/components/site/login/login.component.ts
+++ src/app/components/site/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { User } from '../../../model/user.model';
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [ LoginService ]
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LoginComponent {
+  constructor(
+    private router: Router,
+    private ngZone: NgZone
+  ){
   }
-
 }
+  /* public user : User;
+ 
+  constructor(private loginService: LoginService) {
+      this.user = new User();
+  }
+ 
+  validateLogin() {
+    if(this.user.username && this.user.password) {
+        this.loginService.validateLogin(this.user).subscribe(result => {
+        console.log('result is ', result);
+      }, error => {
+        console.log('error is ', error);
+      });
+    } else {
+        alert('enter user name and password');
+    }
+  }
+ 
+} */
